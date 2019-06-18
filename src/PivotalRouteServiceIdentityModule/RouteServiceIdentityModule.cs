f3cc7ec4cd6ed25698f2cb9e1acf1740371c4f3c
Alfus Inigo Jaganathan <alfusinigo.j@gmail.com>
2019-06-18 13:48:31 -0400

    Fixed case sensitiveness issue

--- src/PivotalRouteServiceIdentityModule/RouteServiceIdentityModule.cs
+++ src/PivotalRouteServiceIdentityModule/RouteServiceIdentityModule.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Security.Claims;
 using System.Web;
+using System.Linq;
 
 namespace Pivotal.RouteServiceIdentityModule
 {
     public class RouteServiceIdentityModule : IHttpModule
     {
         const string CF_IDENTITY_HEADER = "X-Cf-Identity";
-        const string CF_IMPERSONATED_IDENTITY_HEADER = "X-Cf-ImpersonatedIdentity";
+        const string CF_IMPERSONATED_IDENTITY_HEADER = "X-Cf-Impersonated-Identity";
 
 
         public void Init(HttpApplication context)
@@ -19,13 +20,16 @@ namespace Pivotal.RouteServiceIdentityModule
         {
             var context = ((HttpApplication) sender).Context;
 
-            var identityHeader = context.Request.Headers.Get(CF_IMPERSONATED_IDENTITY_HEADER) 
-                                    ?? context.Request.Headers.Get(CF_IDENTITY_HEADER);
+            var isImpersonatedUserHeaderExists = context.Request.Headers.AllKeys.Contains(CF_IMPERSONATED_IDENTITY_HEADER);
 
-            if (identityHeader != null)
+            var identityHeader = isImpersonatedUserHeaderExists
+                                    ? context.Request.Headers.Get(CF_IMPERSONATED_IDENTITY_HEADER) 
+                                        : context.Request.Headers.Get(CF_IDENTITY_HEADER);
+
+            if (!String.IsNullOrWhiteSpace(identityHeader))
             {
                 var nameClaim = new Claim(ClaimTypes.Name, identityHeader);
-                var identity = new ClaimsIdentity(new[] { nameClaim }, "RouteService");
+                var identity = new ClaimsIdentity(new[] { nameClaim }, isImpersonatedUserHeaderExists ? "RouteService-Impersonated" : "RouteService");
                 context.User = new ClaimsPrincipal(identity);
             }
         }
